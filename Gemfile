c6be7529841614465387910209ae7833d825db41
brunobatista25 <brunobatista101@gmail.com>
2018-07-06 17:25:50 -0300

    editando lista de usuarios

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '4.2.5'
 
 gem 'rails_real_favicon'
 
+gem 'kaminari'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.order(:name).page(params[:page]).per(10)
   end
 
   # GET /users/1
