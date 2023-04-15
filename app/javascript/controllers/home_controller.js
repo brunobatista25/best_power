83f0c2a2d606c99511f5a091537c56960f12f393
Bruno Batista <brunobatista101@gmail.com>
2023-04-15 17:50:41 -0300

    adicionando home

---
+++ app/javascript/controllers/home_controller.js
@@ -0,0 +1,7 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="home"
+export default class extends Controller {
+  connect() {
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
   get 'home/index'
 
-  get 'treinamento/home'
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-    #busca elemento
-    get 'buscaelementos/links'
-    get 'buscaelementos/inputsetextfield'
-    get 'buscaelementos/botoes'
-    get 'buscaelementos/textfields'
-    get 'buscaelementos/radioecheckbox'
-    get 'buscaelementos/dropdowneselect'
-    get 'buscaelementos/textos'
-    get 'buscaelementos/table'
-    get 'buscaelementos/success'
-    get 'buscaelementos/notfound'
-    get 'buscaelementos/badrequest'
-    get 'buscaelementos/internalservererror'
 end
